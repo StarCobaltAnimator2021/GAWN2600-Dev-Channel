d95eee540291fc7bf7b610f12df359262171befb
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2023-12-31 14:46:58 +0700

    just not a day release

--- Start.bat
+++ Start.bat
@@ -1,5 +1,5 @@
 @echo off
 echo GAWN2600 Pro Insider Preview
-echo Evaluation copy. Build 22533.rs_prerelease.231108-2050
+echo Evaluation copy. Build 22538.rs_prerelease.231121-1251
 echo:
 npm start
