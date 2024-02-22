6a26ab0f2a6d3c4a89f34055e6c4c67d0314c0ce
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2024-02-23 02:13:55 +0700

    making sure i didn't forget anything :100:

--- Start.bat
+++ Start.bat
@@ -1,5 +1,6 @@
 @echo off
-echo GAWN2600 Pro Insider Preview
-echo Evaluation copy. Build 22543.rs_prerelease.231231-1524
+echo:
+echo GAWN2600 Pro
+echo Evaluation copy. Build 22557.zn_release.240221-0122
 echo:
 npm start
