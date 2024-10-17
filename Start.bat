37cd21e89dd7e327cfe59554971a371da1c50004
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2024-10-17 19:10:58 +0700

    The New Hard Times

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo GAWN2600 Pro Insider Preview
-echo Evaluation copy. Build 25926.rs_prerelease.241010-0828
+echo Evaluation copy. Build 25931.rs_prerelease.241016-1042
 echo:
 npm start
---
+++ server/pages/checkver.bat
@@ -0,0 +1,19 @@
+@Echo off
+title GoAniNathanYellowStar9600 Version Checker
+echo GAWN2600 Pro Insider Preview
+echo Version 2.1.1 (Build 25931.rs_prerelease.241016-1042)
+echo:
+echo New Features and Changes
+echo:
+echo 1. The Wayne Nathaniel Celestials Theme song, along with the intro sound effects, has been integrated.
+echo (Converted from OGG to MP3 through Audacity, before adding it)
+echo:
+echo 2. server/pages/Checkver.bat now uses the correct name type instead of caps (CHECKVER.BAT)
+echo:
+pause
+cls
+echo Bug Fixes.
+echo:
+echo No bugfixes were implemented onto this build.
+echo:
+pause
