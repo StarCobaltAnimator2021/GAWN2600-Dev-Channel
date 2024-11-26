0d98b5570a387245f2f9aaf6eca06e6e255b420d
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2024-11-26 11:15:35 +0700

    still no new features

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo GAWN2600 Pro Insider Preview
-echo Evaluation copy. Build 25951.rs_prerelease.241107-2020
+echo Evaluation copy. Build 25967.rs_prerelease.241124-2112
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,11 +1,12 @@
 @Echo off
 title GoAniNathanYellowStar9600 Version Checker
 echo GAWN2600 Pro Insider Preview
-echo Version 2.1.1 (Build 25951.rs_prerelease.241107-2020)
+echo Version 2.1.1 (Build 25967.rs_prerelease.241124-2112, 25967.1000)
+echo Full buildtag: 25967.1000.rs_prerelease.241124-2112
 echo:
 echo New Features and Changes
 echo:
-echo Slightly changed Aspire Metro Grace UI Color.
+echo Slight change to checkver.bat
 echo:
 pause
 cls
