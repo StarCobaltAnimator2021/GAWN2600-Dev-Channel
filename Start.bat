4d7e5e0474e03bd0c1775c3bf184295e91abea6e
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2024-12-25 18:11:58 +0700

    ho ho ho ho

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo GAWN2600 Pro Insider Preview
-echo Evaluation copy. Build 25977.rs_prerelease.241126-1416
+echo Evaluation copy. Build 25982.rs_prerelease.241220-1840
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,12 +1,12 @@
 @Echo off
 title GoAniNathanYellowStar9600 Version Checker
 echo GAWN2600 Pro Insider Preview
-echo Version 2.1.1 (Build 25977.rs_prerelease.241126-1416, 25977.1010)
-echo Full buildtag: 25977.1010.rs_prerelease_flt.241211-1705
+echo Version 2.1.1 (Build 25982.rs_prerelease.241220-1840, 25982.1000)
+echo Full buildtag: 25982.1000.rs_prerelease.241220-1840
 echo:
 echo New Features and Changes
 echo:
-echo The UI has been changed into Christmas Theme due to December is near.
+echo No New Features were included in this build.
 echo:
 pause
 cls
