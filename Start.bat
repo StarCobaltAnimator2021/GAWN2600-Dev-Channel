153370001a8ab030a0d51fa7d895c4705684828f
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-01-27 11:12:13 +0700

    no new ss

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo GAWN2600 Pro Insider Preview
-echo Evaluation copy. Build 26002.rs_prerelease.250120-2153
+echo Evaluation copy. Build 26010.rs_prerelease.250125-1758
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,14 +1,12 @@
 @Echo off
 title GoAniNathanYellowStar9600 Version Checker
 echo GAWN2600 Pro Insider Preview
-echo Version 2.1.1 (Build 26002.rs_prerelease.250120-2153, 26002.1000)
-echo Full buildtag: 26002.1000.rs_prerelease.250120-2153
+echo Version 2.1.1 (Build 26010.rs_prerelease.250125-1758, 26010.1000)
+echo Full buildtag: 26010.1000.rs_prerelease.250125-1758
 echo:
 echo New Features and Changes
 echo:
-echo 1. New GVL Characters, Ethan Nate, has been added.
-echo:
-echo 2. Slightly Changed the UI Color Interface.
+echo No New Features were included in this build.
 echo:
 pause
 cls
